71b905bb9259f5fdab7e7dfec93ecef2b22064ac
Balazs Valkony (XC/ENG4-Bp) <balazs.valkony@hu.bosch.com>
2025-09-30 21:15:29 +0200

    refactor: extract DxSpot and DxClusterFilters interfaces from dxCluster type definition

--- src/types/dxCluster.ts
+++ src/types/dxCluster.ts
@@ -9,7 +9,41 @@ export interface ScaleTick {
   label?: string;
 }
 
-export interface LayoutSpot extends import('../store/dxCluster').DxSpot {
+export interface DxSpot {
+  Nr: number;
+  Spotter: string;
+  Spotters: string[]; // Array of all spotters for this callsign/frequency
+  Frequency: string;
+  DXCall: string;
+  Time: string;
+  Date: string;
+  Beacon: boolean;
+  MM: boolean;
+  AM: boolean;
+  Valid: boolean;
+  EQSL?: boolean;
+  LOTW?: boolean;
+  LOTW_Date?: string;
+  DXHomecall: string;
+  Comment: string;
+  Flag: string;
+  Band: number;
+  Mode: string;
+  Continent_dx: string;
+  Continent_spotter: string;
+  DXLocator?: string;
+}
+
+export interface DxClusterFilters {
+  selectedCdx: string[];
+  selectedCde: string[];
+  selectedBand: string;
+  selectedModes: string[];
+  validatedOnly: boolean;
+  pageLength: number;
+}
+
+export interface LayoutSpot extends DxSpot {
   position: number;
   leftOffset: number;
   column: number;
